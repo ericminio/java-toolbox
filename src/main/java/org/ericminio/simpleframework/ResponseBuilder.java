930e9d5301ca885d0d6e9fbbdb00214cda954d55
ericminio <eric.mignot@gmail.com>
2012-07-30 14:26:53 -0400

    +RequestBuilder +ResponseBuilder

---
+++ src/main/java/org/ericminio/simpleframework/ResponseBuilder.java
@@ -0,0 +1,31 @@
+package org.ericminio.simpleframework;
+
+import org.simpleframework.http.Response;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ResponseBuilder {
+    private PrintStream output;
+
+    private ResponseBuilder() {
+    }
+
+    public static ResponseBuilder aResponse() {
+        return new ResponseBuilder();
+    }
+
+    public ResponseBuilder withPrintStream(PrintStream output) {
+        this.output = output;
+        return this;
+    }
+
+    public Response build() throws IOException {
+        Response response = mock(Response.class);
+        when(response.getPrintStream()).thenReturn(output);
+        return response;
+    }
+}
---
+++ src/test/java/org/ericminio/simpleframework/ResponseBuilderTest.java
@@ -0,0 +1,23 @@
+package org.ericminio.simpleframework;
+
+import org.junit.Test;
+import org.simpleframework.http.Response;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.ericminio.simpleframework.ResponseBuilder.aResponse;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class ResponseBuilderTest {
+
+    @Test public void
+    canBeBuiltWithAGivenPrintStream() throws IOException {
+        PrintStream printStream = mock(PrintStream.class);
+        Response response = aResponse().withPrintStream(printStream).build();
+
+        assertThat(response.getPrintStream(), equalTo(printStream));
+    }
+}
